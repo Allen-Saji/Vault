e4415babf8383ba4af0a14032fafe351d3c0b021
Allen-Saji <allensaji04@gmail.com>
2024-12-09 00:23:55 +0530

    add anchor vault

---
+++ anchor_vault/programs/anchor_vault_q3_2024/src/lib.rs
@@ -0,0 +1,43 @@
+use anchor_lang::prelude::*;
+
+declare_id!("5EQKCksmgdFZq5UeQkQM6p2ffPTyEwehsY7j5BRj4KuY");
+
+mod context;
+mod state;
+
+use context::*;
+
+#[program]
+pub mod anchor_vault_q3_2024 {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        ctx.accounts.initialize(&ctx.bumps)?;
+        Ok(())
+    }
+
+    pub fn deposit(ctx: Context<Deposit>, amount: u64) -> Result<()> {
+        ctx.accounts.deposit(amount)?;
+
+        Ok(())
+    }
+
+    pub fn withdraw(ctx: Context<Withdraw>, amount: u64) -> Result<()> {
+        ctx.accounts.withdraw(amount)?;
+
+        Ok(())
+    }
+
+    pub fn close(ctx: Context<Close>) -> Result<()> {
+        ctx.accounts.close()?;
+        
+        Ok(())
+    }
+}
+
+
+
+
+
+
+
